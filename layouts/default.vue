e36864f19b5c6befc3e471ec621995ac38274c89
kamal <kamal.zyel@gmail.com>
2024-06-25 09:19:04 +0700

    deploy

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="space-y-10">
+    <!-- Header -->
+    <div class="w-full flex justify-center py-4 bg-green-700">
+      <!-- Group 1 -->
+      <div class="max-w-screen-lg justify-between flex space-x-4 items-center w-full ">
+        <div>
+          <img class="h-10" src="https://www.lapor.go.id/themes/lapor/assets/images/logo-white.png" alt="">
+        </div>
+        <div class="flex space-x-4 font-medium text-slate-100">
+          <p>Tentang Lapor</p>
+          <p>Statistik</p>
+        </div>
+
+        <div>
+          <button class="px-4 py-2 flex space-x-2 bg-slate-800 text-white rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-route-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 19a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M19 7a2 2 0 1 0 0 -4a2 2 0 0 0 0 4z" /><path d="M14 5a2 2 0 0 0 -2 2v10a2 2 0 0 1 -2 2" /></svg>
+            <p>LACAK</p>
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- Header -->
+    <div class="max-w-screen-lg mx-auto justify-center w-full">
+      <slot />
+    </div>
+    <!-- <AppFooter /> -->
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex space-x-6">
+    <div class="w-2/5 space-y-2">
+      <p class="text-3xl font-bold tracking-wide text-slate-800">Layanan Aspirasi dan Pengaduan Online Rakyat</p>
+      <p class="text-lg font-normal text-slate-700">Sampaikan laporan Anda langsung kepada instansi pemerintah berwenang
+      </p>
+    </div>
+    <div class="w-3/5 bg-white border border-slate-400 p-4 rounded-xl">
+      <p class="text-lg font-medium">Formulir Laporan</p>
+      <hr>
+      <div class="grid gap-4 mt-4">
+        <div>
+          <label for="first_name" class="block mb-1 text-sm font-medium text-gray-900 ">Judul Laporan</label>
+          <input type="text" id="first_name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            placeholder="John" required />
+        </div>
+        <div>
+
+          <label for="message" class="block mb-2 text-sm font-medium text-gray-900">Keterangan</label>
+          <textarea id="message" rows="4"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Ketik isi laporan anda..."></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
